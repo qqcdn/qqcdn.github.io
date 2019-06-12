b2d4daa7354f2a4509b8a992b5beaef00c5d25e0
qqcdn <heiwu789@gmail.com>
2019-06-12 15:28:45 +0800

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,47 +1,33 @@
 
-
 //投诉提示
 function tousu(){
-
 document.writeln("<span class=\"grey f12\"><p>本站不存储任何资源内容，只收集BT种子元数据(例如文件名和文件大小)和磁力链接(BT种子标识符)，并提供查询服务，是一个完全合法的搜索引擎系统。 网站不提供种子下载服务，用户可以通过第三方链接或磁力链接获取到相关的种子资源。本站也不对BT种子真实性及合法性负责，请用户注意甄别！投诉邮箱：tousu@"+ document.domain.replace(/^(m.|www.)/g,'') +"</p></span>");
-
 }
 
 //提示
 function qin(){
-
 document.writeln("<span class=\"red f14\"><font color=\"red\">亲，你知道吗？下载的人越多速度越快，赶快把本页面分享给好友一起下载吧^_^ </font></span><br>");
-
 }
-
 
 //logo
 function logo(){
-
 document.writeln("<div class=\'bobologo mb15\'><a href=\'/\'><img src=\'//cdn.jsdelivr.net/gh/qqcdn/qqcdn.github.io/logo/logo.png\' alt=\'海盗湾BT\' /></a></div>");
-
 }
-
 
 //全站底部
 function foot(){
-
 document.writeln("<div class=\'footer\'><p><a href=\'/new.html\' title=\'最新磁力 \' target=\'_blank\'>最新磁力</a>&nbsp;<a href=\'/tag-1.html\' title=\'热门搜索 \' target=\'_blank\'>热门搜索</a>&nbsp;<a href=\'/weekhot.html\' title=\'一周排行榜 \' target=\'_blank\'>一周排行榜</a>&nbsp;</p></div>");
-
 }
 
 //统计
 function tongji(){
-
 document.writeln("<p style=\'display: none;\'><script type=\'text/javascript\' src=\'//js.users.51.la/5300013.js\'></script></p>");
-
 }
 
 //缓存公告
 function huancun(){
 document.writeln("收录今日热门的磁力链接/BT种子资源,收录今日热门数据会被缓存3分钟左右");
 }
-
 
 //内容页
 function hash(){
@@ -106,57 +92,5 @@ document.writeln("迅雷链接：");
 
 function zhongzifenlei(){
 document.writeln("种子分类：");
-}
-
-function xiazaibtzhongzi(){
-document.writeln("<h3>下载BT种子文件</h3>");
-}
-
-function remensousuo(){
-document.writeln("<h3>热门搜索</h3>");
-}
-
-function wenjianliebiao(){
-document.writeln("<h3>文件列表</h3>");
-}
-
-function cainixihuan(){
-document.writeln("<h3>猜你喜欢</h3>");
-}
-
-function zuixincili(){
-document.writeln("<h3>最新磁力</h3>");
-}
-
-function xiaoshi24(){
-document.writeln("<h3>24小时热门磁力</h3>");
-}
-
-function benzhou(){
-document.writeln("<h3>本周热门磁力</h3>");
-}
-
-function banquantixing(){
-document.writeln("<h3>版权提醒</h3>");
-}
-
-function cili(){
-document.writeln("<h3>磁力链接</h3>");
-}
-
-function xunlei(){
-document.writeln("<h3>迅雷链接</h3>");
-}
-
-function xunleitishi(){
-document.writeln("<span class=\'red\'>复制链接到迅雷、QQ旋风进行下载，或者使用百度云离线下载。</span>");
-}
-
-function btmingzi(){
-document.writeln("<h3>磁力链接/BT种子名称</h3>");
-}
-
-function btjianjie(){
-document.writeln("<h3>磁力链接/BT种子简介</h3>");
 }
 
