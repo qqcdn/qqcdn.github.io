2aef92d2e4ceb6e9ae4587e8839ad880d0f5cc2c
qqcdn <heiwu789@gmail.com>
2019-07-27 13:28:10 +0800

    Create url.js

---
+++ js/url.js
@@ -0,0 +1,28 @@
+(() => {
+    let whiteList = ["cdn.jsdelivr.net"];
+    let replace = "www.505107.com";
+
+    function strMatch(str) {
+        let newStr = str;
+        if (str == "" || !str) return newStr;
+        urlReg = /([a-z0-9]+\.){0,1}[a-z0-9]+?\.(net|org|com|cc|ws|la|me|info|cn|xyz|vip|ru)/gim;
+        while ((res = urlReg.exec(str))) {
+            if (whiteList.indexOf(res[0]) < 0) {
+                newStr = newStr.replace(res[0], replace);
+            }
+        }
+        return newStr;
+    }
+
+    $("html")
+        .find("*")
+        .each((index, ele) => {
+            for (let i = 0; i < ele.childNodes.length; i++) {
+                if (ele.childNodes[i].nodeType === 3) {
+                    ele.childNodes[i].nodeValue = strMatch(
+                        ele.childNodes[i].nodeValue
+                    );
+                }
+            }
+        });
+})();
